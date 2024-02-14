03f3ec0d0a9dd7114a2dde31f107eb993f4cf522
Reza <reza.nasirigerdeh@gmail.com>
2024-02-14 21:52:58 +0100

    run-cifar100 updated

--- reproducibility/run-cifar100.sh
+++ reproducibility/run-cifar100.sh
@@ -22,11 +22,3 @@ else
                     --lr-scheduler cosine_annealing --decay-epochs 150 --decay-multiplier 0.01 \
                     --checkpoint-freq 1 --run 1
 fi
-
-python3 simulate.py --dataset cifar100 --low-resolution --num-classes 100 --model $model \
-                    --optimizer sgd --momentum 0.9 --learning-rate $lr --batch-size $bs --epochs 150 \
-                    --random-hflip --random-crop 32x32-4x4 \
-                    --norm-mean 0.5071,0.4865,0.4409  --norm-std 0.2673,0.2564,0.2762 \
-                    --loss cross_entropy  --weight-decay 0.0005 \
-                    --lr-scheduler cosine_annealing --decay-epochs 150 --decay-multiplier 0.01 \
-                    --checkpoint-freq 1 --run 1
